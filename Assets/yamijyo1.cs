0eb0127b69ad03ff8f1c190bc31bb55b2ece3acb
macbook Air 2010 <pieces.tokyo@gmail.com>
2017-04-22 16:15:33 +0900

    change fire button

--- Assets/yamijyo1.cs
+++ Assets/yamijyo1.cs
@@ -16,16 +16,6 @@ public class yamijyo1 : MonoBehaviour {
 	{
 		Instantiate (explosion, transform.position, transform.rotation);
 	}
-	// Startメソッドをコルーチンとして呼び出す
-	IEnumerator Start ()
-	{
-		while (true) {
-			// 弾をプレイヤーと同じ位置/角度で作成
-			Instantiate (bullet, transform.position, transform.rotation);
-			// 0.05秒待つ
-			yield return new WaitForSeconds (0.5f);
-		}
-	}
 
 	// Update is called once per frame
 	void Update () {
@@ -33,6 +23,11 @@ public class yamijyo1 : MonoBehaviour {
         float y= Input.GetAxisRaw("Vertical");
         Vector2 direction = new Vector2(x, y).normalized;
         GetComponent<Rigidbody2D>().velocity = direction * speed;
+
+		if (Input.GetButtonDown("Fire1")) {
+			Instantiate(bullet, transform.position, transform.rotation);
+		}
+
 	}
 
 
